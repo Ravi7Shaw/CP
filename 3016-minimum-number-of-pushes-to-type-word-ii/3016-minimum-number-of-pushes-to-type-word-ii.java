19300cc8d498351efc0000ad23941ab63bae29c6
Ravi7Shaw <javadevravi1@gmail.com>
2026-07-31 10:43:19 +0530

    Time: 9 ms (94.22%), Space: 48.4 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -2,18 +2,17 @@ class Solution {
     public int minimumPushes(String word) {
         int[] freq = new int[26];
 
-        // Count frequency of each letter
+    
         for (char ch : word.toCharArray()) {
             freq[ch - 'a']++;
         }
 
-        // Sort frequencies
+      
         Arrays.sort(freq);
 
         int pushes = 0;
         int pos = 0;
 
-        // Process from highest frequency to lowest
         for (int i = 25; i >= 0; i--) {
             if (freq[i] == 0) continue;
 
