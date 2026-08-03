837a5b627a0823ca12f0f3ca8f3ceaae448e870a
Ravi7Shaw <javadevravi1@gmail.com>
2026-08-03 11:15:10 +0530

    Time: 3 ms (47.75%), Space: 20 MB (12.79%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+
+class Solution:
+    def predictTheWinner(self, nums):
+
+        @lru_cache(None)
+        def solve(left, right):
+
+            if left == right:
+                return nums[left]
+
+            take_left = nums[left] - solve(left + 1, right)
+            take_right = nums[right] - solve(left, right - 1)
+
+            return max(take_left, take_right)
+
+        return solve(0, len(nums) - 1) >= 0
